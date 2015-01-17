baa5f55d41c708cb536f666f476786e2064e269e
victor foreman <vforeman@scs.howard.edu>
2015-01-17 08:06:09 -0500

    working on parallax fab scroll

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,11 +7,13 @@
 
   });
   var tabs = document.querySelector('paper-tabs');
-
   tabs.addEventListener('core-select', function() {
     console.log("Selected: " + tabs.selected);
   });
-
+  var fab = document.querySelector('paper-fab');
+  fab.addEventListener('scroll', function() {
+    console.log("scroll");
+  });
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
