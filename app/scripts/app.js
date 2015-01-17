ce4079a27ba8d1820fa741b30c8b3ccda8669720
victor foreman <vforeman@scs.howard.edu>
2015-01-17 05:54:41 -0500

    complete header

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,12 @@
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
+
+  });
+  var tabs = document.querySelector('paper-tabs');
+
+  tabs.addEventListener('core-select', function() {
+    console.log("Selected: " + tabs.selected);
   });
 
 // wrap document so it plays nice with other libraries
