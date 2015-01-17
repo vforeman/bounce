ba090e98338df8e403af296dfb1dd36da72b79f2
errolgrannum <errolgrannum@gmail.com>
2015-01-17 13:20:30 -0500

    add functionality for creating new items

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,17 +2,26 @@
   'use strict';
 
   document.addEventListener('polymer-ready', function() {
-    // Perform some behaviour
-    console.log('Polymer is ready to rock!');
+	// Perform some behaviour
+	console.log('Polymer is ready to rock!');
 
   });
   var tabs = document.querySelector('paper-tabs');
   tabs.addEventListener('core-select', function() {
-    console.log("Selected: " + tabs.selected);
+	console.log("Selected: " + tabs.selected);
   });
 
-  console.log(document.querySelector('paper-fab'));
+  var fab = document.querySelector('paper-fab');
+  var newitem = document.querySelector('newitem-input');
 
+  var newiteminput = document.querySelector('newitem-input');
+  var newitemfab = document.querySelector('newitem-fab');
+  newitemfab.addEventListener('newitem-fab-clicked',function(e){
+  	newiteminput.showNewItemInput();
+  });
+  newitemfab.addEventListener('newitem-post-clicked',function(e){
+  	newiteminput.hideNewItemInput();
+  });
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
