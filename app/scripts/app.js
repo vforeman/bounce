2c5ed95ed257c029c8c6352b94638131d61af909
victor foreman <vforeman@scs.howard.edu>
2015-01-17 08:32:19 -0500

    fab position in header

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,10 +10,7 @@
   tabs.addEventListener('core-select', function() {
     console.log("Selected: " + tabs.selected);
   });
-  var fab = document.querySelector('paper-fab');
-  fab.addEventListener('scroll', function() {
-    console.log("scroll");
-  });
+
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
